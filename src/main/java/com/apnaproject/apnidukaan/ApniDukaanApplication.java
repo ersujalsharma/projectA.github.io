3a8a711eedaefba5e63efbe0de5462e8359c9d49
sujalsh <sujalsharma350@gmail.com>
2023-08-31 00:10:21 +0530

    Add existing project files to Git

---
+++ src/main/java/com/apnaproject/apnidukaan/ApniDukaanApplication.java
@@ -0,0 +1,13 @@
+package com.apnaproject.apnidukaan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApniDukaanApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApniDukaanApplication.class, args);
+	}
+
+}
